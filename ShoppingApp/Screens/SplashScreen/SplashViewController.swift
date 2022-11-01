ae3a2bf6f7dc3886835264254504c7f3b50f7cd9
Bartu Gençcan <bartugenccandev@gmail.com>
2022-11-01 16:35:23 +0300

    SplashView Updated

--- ShoppingApp/Screens/SplashScreen/SplashViewController.swift
+++ ShoppingApp/Screens/SplashScreen/SplashViewController.swift
@@ -6,65 +6,49 @@
 //
 
 import UIKit
+import Lottie
 
 class SplashViewController: UIViewController {
     
-    var activityView: UIActivityIndicatorView?
-    
-    var logoImageView: UIImageView = {
-        let imageView = UIImageView()
-        return imageView
-    }()
+    private var animationView: LottieAnimationView?
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         view.backgroundColor = .white
         
-        configureLogoImageView()
-        showActivityIndicator()
+            animationView = .init(name: "36605-shopping-cart")
+          
+          animationView!.frame = view.bounds
+          
+          // 3. Set animation content mode
+          
+          animationView!.contentMode = .scaleAspectFit
+          
+          // 4. Set animation loop mode
+          
+          animationView!.loopMode = .loop
+          
+          // 5. Adjust animation speed
+          
+          animationView!.animationSpeed = 0.5
+          
+          view.addSubview(animationView!)
+          
+          // 6. Play animation
+          
+          animationView!.play()
+        
         
         // Waits 2 seconds and goes to home screen
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2){
-            let onboardVC = OnboardContainerViewController()
-            self.navigationController?.setViewControllers([onboardVC], animated: true)
-            
-            self.activityView?.stopAnimating()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2.5){
+           let onboardVC = OnboardContainerViewController()
+           self.navigationController?.setViewControllers([onboardVC], animated: true)
+
+           self.animationView?.stop()
         }
     }
     
-    func showActivityIndicator(){
-        activityView = UIActivityIndicatorView(style: .large)
-        
-        guard let activityView = activityView else {return}
-        
-        self.view.addSubview(activityView)
-        
-        activityView.snp.makeConstraints { make in
-            make.top.equalTo(logoImageView.snp.bottom).offset(32.0)
-            make.centerX.equalTo(logoImageView)
-        }
-        
-        activityView.startAnimating()
-    }
-    
-    func configureLogoImageView(){
-        logoImageView = UIImageView(frame: CGRectMake(0, 0, 100, 100))
-        logoImageView.image = UIImage(named: "AppIcon")
-        
-        logoImageView.layer.borderWidth = 1
-        logoImageView.layer.masksToBounds = false
-        logoImageView.layer.borderColor = UIColor.black.cgColor
-        logoImageView.layer.cornerRadius = logoImageView.frame.height / 2
-        logoImageView.clipsToBounds = true
-        
-        self.view.addSubview(logoImageView)
-        
-        logoImageView.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
-            make.size.equalTo(360.0)
-        }
-    }
 
 }
